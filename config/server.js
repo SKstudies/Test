5643267120043d5189dafea7bfc39c2725167353
braveunknow <braveunknown123@gmail.com>
2023-10-19 20:25:50 +0530

    m

--- config/server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const { app } = require("../app");
-
-const port = process.env.PORT || 8022;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
